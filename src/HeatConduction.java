da80c7fbeef188b6edbabe8acb2610f5f236f329
Radka Nhapulo <radka.fnhapulo@gmail.com>
2025-04-09 15:49:43 +0300

    Implicit scheme

---
+++ src/HeatConduction.java
@@ -0,0 +1,38 @@
+public class HeatConduction {
+//    Thomas algorithm
+    private static void solveMatrix (int n, double[] a, double[] b, double[] c, double[] f,
+                                     double mu1, double kappa1, double mu2, double kappa2,
+                                     double[] y) {
+        double[] alpha = new double[n+1];
+        double[] beta = new double[n+1];
+
+        alpha[n] = kappa2;
+        beta[n] = mu2;
+
+//        Backward sweep
+        for (int i = n-1; i >= 1; i --) {
+            alpha[i] = a[i] / (c[i] - alpha[i+1] * b[i]);
+            beta[i] = (b[i] * beta[i+1] + f[i]) / (c[i] - alpha[i+1] * b[i]);
+        }
+
+//        Forward substitution
+        y[0] = (mu1 + kappa1 * beta[1]) / (1 - alpha[1] * kappa1);
+        for (int i = 0; i < n; i++) {
+            y[i+1] = alpha[i+1] * y[i] + beta[i+1];
+        }
+
+    }
+
+    //        Implicit scheme solver
+    private static void solveImplicit (int n, double h, double[] x, double[] A, double[] B,
+                                       double[] C, double[] F, double mu1, double kappa1, double mu2,
+                                       double kappa2, double[] lambda, double[] y, double tau, double[] y_old) {
+        for (int i = 1; i < n; i++) {
+            A[i] = - (lambda[i] + lambda[i-1]) / (2*h*h);
+            B[i] = - (lambda[i] + lambda[i+1]) / (2*h*h);
+            C[i] = A[i] + B[i] - 1 / tau;
+            F[i] = - y_old[i] / tau;
+        }
+        solveMatrix(n, A, B, C, F, mu1, kappa1, mu2, kappa2, y);
+    }
+}
